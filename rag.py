ebcd1d1f02e0f7fe9886015910b27191b97430c1
BrokenSpaghetti99 <67410061+BrokenSpaghetti99@users.noreply.github.com>
2024-11-29 14:43:16 +0800

    [refine] changed embedding and preprocessing models

--- rag.py
+++ rag.py
@@ -3,12 +3,16 @@ from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_unstructured import UnstructuredLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores.utils import filter_complex_metadata
 from dotenv import load_dotenv
 import os
+
+from unstructured.partition.pdf import partition_pdf
+from langchain.docstore.document import Document
 
 class ChatPDF:
     vector_store = None
@@ -17,11 +21,12 @@ class ChatPDF:
 
     def __init__(self):
         self.model = ChatOllama(model="llama3.2")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=2056, chunk_overlap=300)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
         self.prompt = PromptTemplate.from_template(
             """
             <s> [INST] You are an expert on networking topics. Use the following context to answer the question. 
-            Ensure that your answer includes *all the relevant steps and details* required to perform the task. 
+            Ensure that your answer includes ALL THE RELEVANT DETAILS AND CODE required to perform the task.
+            You will ALWAYS focus more on the CODE than the text description.
             If you don't know the answer, say you don't know. 
             [/INST] 
             </s> 
@@ -30,6 +35,8 @@ class ChatPDF:
             Answer (include all steps): [/INST]
             """
         )
+        load_dotenv()
+        self.UNSTRUCTURED_API_KEY = os.getenv("UNSTRUCTURED_API_KEY")
 
     def ingest(self, pdf_file_path: str):
         docs = PyPDFLoader(file_path=pdf_file_path).load()
@@ -44,7 +51,7 @@ class ChatPDF:
                 "score_threshold": 0.5,
             },
         )
-
+        
         self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
                       | self.prompt
                       | self.model
